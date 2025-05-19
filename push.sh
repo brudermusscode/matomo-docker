97158aec3f5eb5875b139f93a76a0ad9a49d1411
brudermusscode <git@bruder.business>
2025-05-19 08:34:55 +0000

    Add push.sh file for everyone!

---
+++ push.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "\033[1;38;5;208mCroissant [🥐] will push for you!\033[0m "
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+# Check for Änderungen
+# if [ -z "$(git status --porcelain)" ]; then
+#   echo "[🥐] „Nothing to commit!“"
+#   exit 0
+# fi
+
+
+# Ask for a commit message.
+echo -n "[🥐] „Enter a commit message!“ | ✒️  "
+read COMMIT_MSG
+
+echo "[🥐] „Cool! You say: »$COMMIT_MSG«.“"
+
+# Push everything.
+echo "[🥐] „Let me push now...“"
+git add .
+git commit -m "$COMMIT_MSG"
+git push
+
+echo "[🥐] „All done my friend, see you next time!“"
